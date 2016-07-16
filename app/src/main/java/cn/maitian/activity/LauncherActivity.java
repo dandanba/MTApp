057bbb93918ccf1df01b60670ee35dbf239db463
geng <13641194007@139.com>
2016-07-16 14:52:24 +0800

    Initial commit

---
+++ app/src/main/java/cn/maitian/activity/LauncherActivity.java
@@ -0,0 +1,12 @@
+package cn.maitian.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
