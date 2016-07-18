fb7d195e2a62b13dc87a444b05644a0b41b9a6bb
geng <13641194007@139.com>
2016-07-18 13:15:41 +0800

    add: fabric Crashlytics

--- app/src/main/java/cn/maitian/app/MTApplication.java
+++ app/src/main/java/cn/maitian/app/MTApplication.java
@@ -1,16 +1,14 @@
 package cn.maitian.app;
 
-import android.app.Application;
-
 /**
  * Created by geng on 2016/7/16.
  */
-public class MTApplication extends Application {
+public class MTApplication extends BaseApplication {
+    private static MTApplication sInstance;
+
     public static MTApplication getInstance() {
         return sInstance;
     }
-
-    private static MTApplication sInstance;
 
     @Override
     public void onCreate() {
